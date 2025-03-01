39a757201ba868754ed19691263860152512e636
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2025-03-02 00:37:34 +0900

    10~11 데이터베이스 mysql생성 및 정상출력확인

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,34 @@ import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
+import CircularProgress from "@mui/material/CircularProgress";
 import { styled } from "@mui/material/styles";
 import "./App.css";
 
-const Root = styled(Paper)(({ theme }) => ({
+const Root = styled(Paper)({
   width: "100%",
-  marginTop: theme.spacing(3),
+  marginTop: "16px",
   overflowX: "auto",
-}));
+});
 
 const StyledTable = styled(Table)({
   minWidth: 1080,
 });
 
+const ProgressContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  padding: "20px",
+});
+
 class App extends Component {
   state = {
     customers: [],
+    completed: 0,
   };
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then((res) => {
         if (res) {
@@ -40,45 +49,62 @@ class App extends Component {
       if (!response.ok) {
         throw new Error(`서버 오류: ${response.status}`);
       }
-      return await response.json();
+      const data = await response.json();
+      console.log(data); // 응답을 콘솔에 출력하여 확인
+      return data;
     } catch (error) {
       console.error("API 요청 중 오류 발생:", error);
     }
   };
 
+  progress = () => {
+    this.setState((prevState) => ({
+      completed: prevState.completed >= 100 ? 0 : prevState.completed + 1,
+    }));
+  };
+
   render() {
     return (
-      <>
-        <Root>
-          <StyledTable>
-            <TableHead>
+      <Root>
+        <StyledTable>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers.length > 0 ? (
+              this.state.customers.map((c) => (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              ))
+            ) : (
               <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.customers.length > 0
-                ? this.state.customers.map((c) => (
-                    <Customer
-                      key={c.id}
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}
+                <TableCell colSpan="6">
+                  <ProgressContainer>
+                    <CircularProgress
+                      variant="indeterminate"
+                      value={this.state.completed}
                     />
-                  ))
-                : "로딩 중..."}
-            </TableBody>
-          </StyledTable>
-        </Root>
-      </>
+                  </ProgressContainer>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </StyledTable>
+      </Root>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,41 +1,31 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/any",
-      name: "안중현",
-      birthday: "990704",
-      gender: "남자",
-      gob: "취준생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "홍길이",
-      birthday: "898989",
-      gender: "여자",
-      gob: "프로그랭",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "박박이",
-      birthday: "721212",
-      gender: "남자",
-      gob: "백숙",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
