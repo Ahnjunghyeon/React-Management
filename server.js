99da8d145b336e4efb637397a99c4f0ca4de05de
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2025-03-03 00:01:52 +0900

    15 DB에서삭제, 웹에서삭제 생성

--- server.js
+++ server.js
@@ -26,15 +26,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/customers", (req, res) => {
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
-    res.send(rows);
-  });
+  connection.query(
+    "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.use("/image", express.static("./upload"));
 
 app.post("/api/customers", upload.single("image"), (req, res) => {
-  let sql = "INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)";
+  let sql = "INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)";
   let image = "/image/" + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -44,6 +47,19 @@ app.post("/api/customers", upload.single("image"), (req, res) => {
   connection.query(sql, params, (err, rows, fields) => {
     if (err) {
       console.error("DB 오류:", err);
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
+  });
+});
+
+app.delete("/api/customers/:id", (req, res) => {
+  let sql = "UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?";
+  let params = [req.params.id];
+  connection.query(sql, params, (err, rows, fields) => {
+    if (err) {
+      console.error("삭제 오류:", err);
       res.status(500).send(err);
     } else {
       res.send(rows);
